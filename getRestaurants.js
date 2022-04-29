086b6e02bb4e76289c5e3032433648394108d12a
Nerissa Dorlus <nerissadorlus@Tdas-Pro-3.local>
2022-04-29 15:07:20 -0400

    Made code dryer

--- getRestaurants.js
+++ getRestaurants.js
@@ -1,22 +1,4 @@
-import admin from "firebase-admin"; // E6 imports firebase lib of tools
-
-import serviceAccount from "./credentials.js"; //E6 importing credentials to connect to firebase
-
-//const admin = require("firebase-admin");//ES5
-
-//const serviceAccount = require("./credentials.json");// ES5
-
-admin.initializeApp({
-  //connects to our  firebase project
-  credential: admin.credential.cert(serviceAccount), //creating a certificate from credentials
-});
-
-// we are now connected to our firebase project & related sevices
-
-const db = admin.firestore(); //shortcut to access firestore db
-
-const restaurantsCol = db.collection("restaurants"); // shortcut  to point to collections
-
+import { restaurantsCol } from "./connectDb.js";
 // restaurantsCol
 //   .get() // getting all rrestaurants
 //   .then((snapshot) => {
